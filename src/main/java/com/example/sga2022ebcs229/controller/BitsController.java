f5743722b708f68d9e348e60eb0a1b71a7c8c2de
Makarand Khare <makarand.khare@gmail.com>
2024-10-18 14:31:34 +0530

    commit

--- src/main/java/com/example/sga2022ebcs229/controller/BitsController.java
+++ src/main/java/com/example/sga2022ebcs229/controller/BitsController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.sga2022ebcs229.repository.CustomerRepo;
 import com.example.sga2022ebcs229.service.CustomerService;
 
 @Controller
