a73d1869096ec09769c9fb4358890684a4b7ca54
Makarand Khare <makarand.khare@gmail.com>
2024-10-18 12:07:20 +0530

    Deleted a controller

--- src/main/java/com/example/sga2022ebcs229/controller/BitsController.java
+++ src/main/java/com/example/sga2022ebcs229/controller/BitsController.java
@@ -2,11 +2,64 @@ package com.example.sga2022ebcs229.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.sga2022ebcs229.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.sga2022ebcs229.repository.CustomerRepo;
+import com.example.sga2022ebcs229.service.CustomerService;
 
 @Controller
 public class BitsController {
-        @GetMapping("")
+    
+    //this variable 'cr' is available for all the jsp pages 
+    @ModelAttribute("cr")
+    public String cr(){
+        System.out.println("in cr method");
+        return "© Makarand Khare";
+    }
+
+    @GetMapping("")
     public String home(){
         return "index";
     } 
+
+    @Autowired
+    CustomerRepo repo;    
+
+    @Autowired
+    CustomerService service;
+    
+    @GetMapping("customerReg")
+    public String custRegPage(){
+        return "customerRegistration";
+    }
+
+    @PostMapping("customer")
+    public String addCustomer(Customer customer) {
+        //if JSP variable has different name than customer then use addCustomer(@ModelAttribute("variable_name") Customer customer)
+        service.save(customer);
+        return "customer";
+    }
+
+    @GetMapping("customer/{custid}")
+    public ModelAndView getCustomer(@PathVariable("custid") int id, ModelAndView mv){
+
+        mv.addObject("customer", service.findById(id));
+        mv.setViewName("customer");
+
+        return mv;
+    }
+
+    @GetMapping("customers")
+    public String getCustomers() {
+        service.getCustomers();
+        return "index";
+    }
+    
+     
 }
